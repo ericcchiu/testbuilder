3e94043b441d50f25de6370343aa591820b9fb73
ericcchiu <eric.chn.chiu@gmail.com>
2018-10-06 06:28:46 +0800

    [Refactor] Refactor logic for switch network to be more inclusive.

--- detectNetwork.js
+++ detectNetwork.js
@@ -12,40 +12,41 @@ var detectNetwork = (cardNumber) => {
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  let network = "Invalid Card number. Please try again!";
+  let network = "Unable to detect network. Invalid card number. Please try again!";
   let cardNum = cardNumber.split("");
-
+  
+  // Check if network is Diner's club 
   if (cardNum[0] === '3' && (cardNum[1] === '8' || cardNum[1] === '9')) {
   	if (cardNum.length === 14) {
       network = "Diner's Club";
-  	}
-  }
-  else if (cardNum[0] === '3' && (cardNum[1] === '4' || cardNum[1] === '7')) {
+    }
+  // Check if network is American Express
+  } else if (cardNum[0] === '3' && (cardNum[1] === '4' || cardNum[1] === '7')) {
   	if (cardNum.length === 15) {
   		network = "American Express";
-  	}
-  }
-  else if (cardNum[0] === '4') {
+    }
+  // Check if network is Visa 
+  } else if (cardNum[0] === '4') {
   	if (cardNum.length === 13 || cardNum.length === 16 || cardNum.length === 19) {
   		network = "Visa";
-  	}
-  }
-  else if (cardNum[0] === '5' && (cardNum[1] === '1' || cardNum[1] === '2' || cardNum[1] === '3' || cardNum[1] === '4' || cardNum[1] === '5')) {
+    }
+  // Check if network is MasterCard
+  } else if (cardNum[0] === '5' && (cardNum[1] === '1' || cardNum[1] === '2' || cardNum[1] === '3' || cardNum[1] === '4' || cardNum[1] === '5')) {
   	if (cardNum.length === 16) {
   		network = "MasterCard";
-  	}
-  }
-  else if (cardNumber.substr(0,4) === '6011' || (Number(cardNumber.substr(0,3)) >= 644 && Number(cardNumber.substr(0,3) <= 649)) || cardNumber.substr(0,2) === '65') {
+    }
+  // Check if network is Discover 
+  } else if (cardNumber.substr(0,4) === '6011' || (Number(cardNumber.substr(0,3)) >= 644 && Number(cardNumber.substr(0,3) <= 649)) || cardNumber.substr(0,2) === '65') {
   	if (cardNum.length === 16 || cardNum.length === 19) {
   		network = "Discover";
-  	}
-  }
-  else if (cardNumber.substr(0,4) === '5018' || cardNumber.substr(0,4) === '5020'  || cardNumber.substr(0,4) === '5038' || cardNumber.substr(0,4) === '6304') {
+    }
+  // Check if network is Maestro
+  } else if (cardNumber.substr(0,4) === '5018' || cardNumber.substr(0,4) === '5020'  || cardNumber.substr(0,4) === '5038' || cardNumber.substr(0,4) === '6304') {
   	if (cardNum.length <= 19 || cardNum.length >= 12) {
   		network = "Maestro";
-  	}
-  }
-  else if ((Number(cardNumber.substr(0,6)) >= 622126 && Number(cardNumber.substr(0,6)) <= 622925)  || (Number(cardNumber.substr(0,3)) >= 624 && Number(cardNumber.substr(0,3)) <= 626) || (Number(cardNumber.substr(0,4)) >= 6282 && Number(cardNumber.substr(0,4)) <= 6288)){
+    }
+  // Check if network is Union Pay 
+  } else if ((Number(cardNumber.substr(0,6)) >= 622126 && Number(cardNumber.substr(0,6)) <= 622925)  || (Number(cardNumber.substr(0,3)) >= 624 && Number(cardNumber.substr(0,3)) <= 626) || (Number(cardNumber.substr(0,4)) >= 6282 && Number(cardNumber.substr(0,4)) <= 6288)){
   	if (cardNum.length <= 19 || cardNum.length >= 16) {
   		network = "Union Pay";
   	}
@@ -53,9 +54,12 @@ var detectNetwork = (cardNumber) => {
   //Visa and Switch have some overlap in card numbers.  Choose network with longer prefix. In this situation, 
   //Switch will always override the Visa number.
   //By excluding Switch from the else statement, it will override the Visa numbers.
-  let switchPrefixes = ['4903', '4905', '4911', '4936', '564182', '633110', '6333', '6759'];
-  for (let i = 0; i < switchPrefixes.length; i++) {
-    if (switchPrefixes[i] === cardNumber.substr(0, switchPrefixes[i].length)) {
+  let switchPre = ['4903', '4905', '4911', '4936', '564182', '633110', '6333', '6759'];
+  // Iterate over list of switch prefixes 
+  for (let i = 0; i < switchPre.length; i++) {
+    // Check if card number prefix is equivalent to switch prefix 
+    if (switchPre[i] === cardNumber.substr(0, switchPre[i].length)) {
+      // Check the length of the card number
     	if (cardNum.length === 19 || cardNum.length === 16 || cardNum.length === 18) {
     		network = 'Switch';
     	}
